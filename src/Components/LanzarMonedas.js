dbf9c3b554fabe63a340c695fa9e7ceda9d20815
Brianvera97 <Bryanverac@gmail.com>
2024-01-02 19:41:27 -0300

    feat: Update

---
+++ src/Components/LanzarMonedas.js
@@ -0,0 +1,32 @@
+function tossCoin() {
+    return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+function fiveHeads() {
+    return new Promise((resolve, reject) => {
+        let headsCount = 0;
+        let attempts = 0;
+
+        while (headsCount < 5 && attempts < 100) {
+            attempts++;
+            let result = tossCoin();
+            if (result === "heads") {
+                headsCount++;
+            } else {
+                headsCount = 0;
+            }
+        }
+
+        if (headsCount === 5) {
+            resolve(`It took ${attempts} tries to flip five "heads"`);
+        } else {
+            reject("Failed to flip five heads in a row");
+        }
+    });
+}
+
+fiveHeads()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+console.log("When does this run now?");
